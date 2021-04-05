a507a8c1ec372dc073df300fa7957baeb93fb399
Raytwo <Raytwost@gmail.com>
2021-04-06 01:34:38 +0200

    Add dummy "register_callback" API method

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,12 @@ mod hash40;
 pub use hash40::{hash40, Hash40};
 
 extern "C" {
+    fn arcrop_register_callback(hash: Hash40);
     fn arcrop_load_file(hash: Hash40, buffer: *mut u8, length: usize);
+}
+
+pub fn register_callback<H: Into<Hash40>>(hash: H) {
+    unsafe { arcrop_register_callback(hash.into()) }
 }
 
 pub fn load_original_file<H, B>(hash: H, mut out_buffer: B)
