7773234234d7b7846fe2ffb6b1b004c2e86ecd22
Raytwo <Raytwost@gmail.com>
2021-04-05 23:16:10 +0200

    Implement load_original_file

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,15 @@ mod hash40;
 pub use hash40::{hash40, Hash40};
 
 extern "C" {
-    fn arcrop_load_file(hash: Hash40, buffer: *mut u8, length: u32);
+    fn arcrop_load_file(hash: Hash40, buffer: *mut u8, length: usize);
 }
 
-pub fn load_original_file<H, B>(hash: H, buffer: B)
+pub fn load_original_file<H, B>(hash: H, mut out_buffer: B)
 where
     H: Into<Hash40>,
-    B: AsRef<[u8]>,
+    B: AsMut<[u8]>,
 {
-    unimplemented!()
+    let buf = out_buffer.as_mut();
+
+    unsafe { arcrop_load_file(hash.into(), buf.as_mut_ptr(), buf.len()) }
 }
