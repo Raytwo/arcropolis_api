de5f0538fc3b0b2ea61fd13fdfeda59832245ca9
Raytwo <Raytwost@gmail.com>
2021-04-13 20:10:58 +0200

    Add out_size to CallbackFn and have it return a bool

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,12 @@ pub use hash40::{hash40, Hash40};
 
 extern "C" {
     fn arcrop_register_callback(hash: u64, length: usize, cb: CallbackFn);
-    fn arcrop_load_file(hash: u64, buffer: *mut u8, length: usize);
+    fn arcrop_load_file(out_size: *mut usize, hash: u64, buffer: *mut u8, length: usize);
     fn arcrop_api_version();
 }
 
 // Hash, out_buffer, length
-pub type CallbackFn = extern "C" fn(u64, *mut u8, usize);
+pub type CallbackFn = extern "C" fn(*mut usize, u64, *mut u8, usize) -> bool;
 
 /// /!\ TEMP IMPLEMENTATION, SUBJECT TO CHANGE /!\  
 /// Register your callback to ARCropolis.  
@@ -25,14 +25,16 @@ pub fn register_callback<H: Into<Hash40>>(hash: H, length: usize, cb: CallbackFn
 /// /!\ TEMP IMPLEMENTATION, SUBJECT TO CHANGE /!\  
 /// Provide the original data.arc file, or the user's if one is present.  
 /// To be called from within your callback function if you desire to work on an existing file. Make sure the buffer is at least as large as the size you provided when registering.
-pub fn load_original_file<H, B>(hash: H, mut out_buffer: B)
+pub fn load_original_file<H, B>(hash: H, mut buffer: B)
 where
     H: Into<Hash40>,
     B: AsMut<[u8]>,
 {
-    let buf = out_buffer.as_mut();
+    let buf = buffer.as_mut();
 
-    unsafe { arcrop_load_file(hash.into().as_u64(), buf.as_mut_ptr(), buf.len()) }
+    let mut out_size: usize = 0;
+
+    unsafe { arcrop_load_file(&mut out_size, hash.into().as_u64(), buf.as_mut_ptr(), buf.len()) }
 }
 
 /// /!\ TEMP IMPLEMENTATION, SUBJECT TO CHANGE /!\  
