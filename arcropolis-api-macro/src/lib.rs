059bb961eaf5c8c781bcc6d99479f2ac2a08c773
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-05-10 15:17:08 -0400

    Add ext_callback attribute macro

--- arcropolis-api-macro/src/lib.rs
+++ arcropolis-api-macro/src/lib.rs
@@ -87,3 +87,40 @@ pub fn stream_callback(_: TokenStream, item: TokenStream) -> TokenStream {
         #func
     ).into()
 }
+
+#[proc_macro_attribute]
+pub fn ext_callback(_: TokenStream, item: TokenStream) -> TokenStream {
+    let func = syn::parse_macro_input!(item as syn::ItemFn);
+
+    let ident = &func.sig.ident;
+    let vis = &func.vis;
+
+    quote!(
+        #vis mod #ident {
+            pub(crate) fn install<H: Into<::arcropolis_api::Hash40>>(extension: H) {
+                ::arcropolis_api::register_extension_callback(extension.into(), callback)
+            }
+
+            extern "C" fn callback(
+                hash: u64,
+                data: *mut u8,
+                size: usize,
+                out_size: &mut usize
+            ) -> bool {
+                let data = unsafe { std::slice::from_raw_parts_mut(data, size) };
+                
+                match CB(hash, data) {
+                    Some(size) => {
+                        *out_size = size;
+                        true
+                    }
+                    None => false
+                }
+            }
+
+            const CB: fn(u64, &mut [u8]) -> Option<usize> = super::#ident;
+        }
+
+        #func
+    ).into()
+}
