3c21cbe5f25642660f2546408eb4b35ebfb6d572
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-05-10 15:04:28 -0400

    Move ExtCallbackFn to arcropolis_api

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(str_strip)]
+#![allow(stable_features)]
 
 mod hash40;
 pub use hash40::{hash40, Hash40};
@@ -14,6 +15,7 @@ extern "C" {
     fn arcrop_load_file(hash: u64, buffer: *mut u8, length: usize, out_size: &mut usize) -> bool;
     fn arcrop_api_version() -> &'static ApiVersion;
     fn arcrop_require_api_version(major: u32, minor: u32);
+    fn arcrop_register_extension_callback(hash: Hash40, cb: ExtCallbackFn);
 }
 
 // Hash, out_buffer, length, out_size
@@ -21,16 +23,27 @@ pub type CallbackFn = extern "C" fn(u64, *mut u8, usize, &mut usize) -> bool;
 // Hash, out_path, out_size
 pub type StreamCallbackFn = extern "C" fn(u64, *mut u8, &mut usize) -> bool;
 
+// Extension hash, out_buffer, length, out_size
+pub type ExtCallbackFn = extern "C" fn(Hash40, *mut u8, usize, &mut usize) -> bool;
+
 pub fn register_callback<H: Into<Hash40>>(hash: H, length: usize, cb: CallbackFn) {
     unsafe { arcrop_register_callback(hash.into().as_u64(), length, cb) }
 }
 
-pub fn register_stream_callback<H>(hash: H, cb: StreamCallbackFn) 
+pub fn register_stream_callback<H>(hash: H, cb: StreamCallbackFn)
 where
-    H: Into<Hash40>
+    H: Into<Hash40>,
 {
     require_api_version(1, 1);
     unsafe { arcrop_register_callback_with_path(hash.into().as_u64(), cb) }
+}
+
+pub fn register_extension_callback<H>(hash: H, cb: ExtCallbackFn)
+where
+    H: Into<Hash40>,
+{
+    require_api_version(1, 2);
+    unsafe { arcrop_register_extension_callback(hash.into(), cb) }
 }
 
 pub fn load_original_file<H, B>(hash: H, mut buffer: B) -> Option<usize>
@@ -42,7 +55,14 @@ where
 
     let mut out_size: usize = 0;
 
-    let success = unsafe { arcrop_load_file(hash.into().as_u64(), buf.as_mut_ptr(), buf.len(), &mut out_size) };
+    let success = unsafe {
+        arcrop_load_file(
+            hash.into().as_u64(),
+            buf.as_mut_ptr(),
+            buf.len(),
+            &mut out_size,
+        )
+    };
 
     if success {
         Some(out_size)
